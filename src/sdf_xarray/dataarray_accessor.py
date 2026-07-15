292e4752976da062c6699a8534c937340acc0b78
Joel Adams <joelucadams@gmail.com>
2026-07-15 13:57:55 +0200

    update dataarray plot accessor to remove x or y check

--- src/sdf_xarray/dataarray_accessor.py
+++ src/sdf_xarray/dataarray_accessor.py
@@ -69,7 +69,6 @@ class EpochAccessor:
         """
         dims = self._obj.dims
         is_time_dim_present = "time" in dims
-        is_x_or_y_specified_in_kwargs = "x" in kwargs or "y" in kwargs
 
         if len(dims) == 1:
             return self._obj.plot(*args, **kwargs)
@@ -80,11 +79,7 @@ class EpochAccessor:
             updated_kwargs.setdefault("y", dims[1])
             return self._obj.plot(*args, **updated_kwargs)
 
-        if (
-            len(dims) == 3
-            and not is_time_dim_present
-            and not is_x_or_y_specified_in_kwargs
-        ):
+        if len(dims) == 3 and not is_time_dim_present:
             return voxel_plot(self._obj, *args, **kwargs)
 
         return self._obj.plot(*args, **kwargs)
